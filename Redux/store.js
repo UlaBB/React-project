09288d37ea8f8be2ee527374bed346b4b16f90a9
Ula Buczak <mankowskau@gmail.com>
2020-10-30 19:51:51 +0100

    Add filtering

--- Redux/store.js
+++ Redux/store.js
@@ -2,6 +2,7 @@ import { combineReducers, createStore } from 'redux';
 import initialStoreData from '../src/data/dataStore.js';
 import columnsReducer from '../src/redux/columnsRedux';
 import cardsReducer from '../src/redux/cardsRadux';
+import searchStringRedux from '../src/redux/searchStringRedux';
 
 // define initial state and shallow-merge initial data
 const initialState = {
@@ -16,6 +17,7 @@ const initialState = {
 const reducers = {
   columns: columnsReducer,
   cards: cardsReducer,
+  searchString: searchStringRedux,
 };
 
 // add blank reducers for initial state properties without reducers
--- src/redux/columnsRedux.js
+++ src/redux/columnsRedux.js
@@ -8,6 +8,7 @@ export const getColumnsForList = ({ columns }, listId) => columns.filter(column 
 const reducerName = 'columns';
 const createActionName = name => `app/${reducerName}/${name}`;
 
+
 // action types
 export const ADD_COLUMN = createActionName('ADD_COLUMN');
 
