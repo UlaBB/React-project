fef053a33958c0c924953e9ed433410b4105336f
Ula Buczak <mankowskau@gmail.com>
2020-10-31 14:50:36 +0100

    Add Container

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,6 +6,7 @@ import Column from '../Column/ColumnContainer';
 import { settings } from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
 import Creator from '../Creator/Creator';
+import Container from '../Container/Container';
 
 
 class List extends React.Component {
@@ -59,6 +60,7 @@ class List extends React.Component {
         <div className={styles.creator}>
           <Creator text={settings.columnCreatorText} action={addColumn} />
         </div>
+        <Container />
       </section>
     );
   }
